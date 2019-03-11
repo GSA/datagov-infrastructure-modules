f6f5c06a13c6eb57fc7b27511109ed4a1f2f4c8c
Aaron D Borden <adborden@a14n.net>
2019-03-11 13:10:54 -0700

    [jumpbox] a few more provision items
    Make it so you can login, activate your virtualenv and start deploying.

--- jumpbox/bin/provision.sh
+++ jumpbox/bin/provision.sh
@@ -7,5 +7,21 @@ set -o nounset
 sudo apt update
 
 sudo apt install -y \
+  git \
   python3-dev \
   python-virtualenv
+
+
+# Clone the repo
+git clone https://github.com/GSA/datagov-deploy.git
+
+# Create a virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install -U setuptools
+pip install -r datagov-dedupe/requirements.txt
+
+# Configure SSH to ignore host keys
+cat <<EOF > ~/.ssh/config
+StrictHostKeyChecking=no
+EOF
